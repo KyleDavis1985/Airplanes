22dcf224e539e3d283673ef470916457cf1c4bc5
Kyle Davis <kyle.davis@ucdenver.edu>
2022-10-21 13:57:18 -0600

    initialized front end

--- airplanes-frontend/src/App.js
+++ airplanes-frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
+  const [genres, updateGenres] = useState([])
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get('http://localhost:3001/genres')
+      updateGenres(response.data)
+    }
+    apiCall()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Genres</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
