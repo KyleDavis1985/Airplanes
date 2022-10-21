f892a5dec1f68b250516b87374d7411f2f32cb44
Kyle Davis <kyle.davis@ucdenver.edu>
2022-10-21 08:56:05 -0600

    added some CRUD to server.js

--- models/airplane.js
+++ models/airplane.js
@@ -2,10 +2,12 @@ const { Schema } = require('mongoose')
 
 const airplaneSchema = new Schema(
   {
-    title: { type: String, required: true },
+    model: { type: String, required: true },
     description: { type: String, required: true },
     top_speed: { type: String, required: true },
-    stealth: { type: Boolean, required: true },
+    stealth: { type: String },
+    date_deployed: { type: String },
+    unit_cost: { type: String, required: true },
     url: { type: String, required: true },
     genre: { type: Schema.Types.ObjectId, ref: 'Genre' }
   },
--- server.js
+++ server.js
@@ -16,6 +16,32 @@ app.get('/', (req, res) => {
 
 //routes
 
+// create genre - POST ROUTE
+app.post('/genres', async (req, res) => {
+  let createdGenre = await Genre.create(req.body)
+  res.json(createdGenre)
+})
+
+// pull all genres - GET ROUTE
+app.get('/genres', async (req, res) => {
+  let allGenres = await Genre.find({})
+  res.json(allGenres)
+})
+
+// create an airplane --> POST
+app.post('/airplanes', async (req, res) => {
+  let exampleGenreId = '6352acfb81fc0d82a06ae986'
+  const requestBody = { ...req.body, genre: exampleGenreId }
+  let createdAirplane = await Airplane.create(requestBody)
+  res.json(createdAirplane)
+})
+
+// pull all airplanes - GET
+app.get('/airplanes', async (req, res) => {
+  const allAirplanes = await Airplane.find({})
+  res.json(allAirplanes)
+})
+
 app.listen(PORT, () => {
   console.log(`Express server listening on port: ${PORT}`)
 })
